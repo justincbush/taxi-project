e8b726976447568a5f994e35af8695d0e578cca9
justincbush <jcbush@gmail.com>
2014-09-10 15:51:50 -0400

    last commit was incorrect version

--- src/merge_taxi_data.py
+++ src/merge_taxi_data.py
@@ -20,20 +20,20 @@ for argument in sys.argv[1:]:
     print 'loading ' + tripfilename
     trip_data = pd.read_csv(tripfilename, compression='gzip')
     print 'loaded ' + tripfilename,
-    print 'took', (time.clock() - local_start)*3600, 'seconds'
+    print ' took', time.clock() - local_start, 'seconds'
     local_start = time.clock()
     print 'loading ' + farefilename
     fare_data = pd.read_csv(farefilename, compression='gzip')
     print 'loaded ' + farefilename,
-    print 'took', (time.clock() - local_start)*3600, 'seconds'
+    print ' took', time.clock() - local_start, 'seconds'
     
-    print 'total of', (time.clock() - global_start)*3600, 'seconds so far'
+    print 'total of', time.clock() - global_start, 'seconds so far'
     
     local_start = time.clock()
     trip_data.rename(columns=lambda x: x.strip(), inplace = True)
     fare_data.rename(columns=lambda x: x.strip(), inplace = True)
     print 'columns renamed',
-    print 'took', (time.clock() - local_start)*3600, 'seconds'
+    print ' took', time.clock() - local_start, 'seconds'
     
     local_start = time.clock()
     print 'merging data'    
@@ -41,24 +41,24 @@ for argument in sys.argv[1:]:
     trip_data = None
     fare_data = None
     print 'data merged',
-    print 'took', (time.clock() - local_start)*3600, 'seconds'
+    print ' took', time.clock() - local_start, 'seconds'
    
-    print 'total of', (time.clock() - global_start)*3600, 'seconds so far'
+    print 'total of', time.clock() - global_start, 'seconds so far'
     
     local_start = time.clock()
     print 'changing time formats'
     taxi_data['pickup_datetime'] = pd.to_datetime(taxi_data['pickup_datetime'])
     taxi_data['dropoff_datetime'] = pd.to_datetime(taxi_data['dropoff_datetime'])
     print 'time formats changed',
-    print 'took', (time.clock() - local_start)*3600, 'seconds'
+    print ' took', time.clock() - local_start, 'seconds'
     
     local_start = time.clock()
     print 'writing file'
     taxi_data.to_csv(taxifilename)
     print 'file written',
-    print 'took', (time.clock() - local_start)*3600, 'seconds'
+    print ' took', time.clock() - local_start, 'seconds'
     
-    print 'total of', (time.clock() - global_start)*3600, 'seconds'
+    print 'total of', time.clock() - global_start, 'seconds'
     
 
     
